6b0160c2af5b413c0dc9057fedfff2ed20fdc152
Joseph <josemanangan@gmail.com>
2024-07-24 14:18:38 -0400

    save

---
+++ StephenToubSample/ProgramClassRefTypeSample.cs
@@ -0,0 +1,23 @@
+﻿//public class Program
+//{
+//    public static void Main()
+//    {
+//        // a classic reference type example
+//        SampleClass sampleClass = new SampleClass();
+//        sampleClass.IntegerProperty = 1;
+
+//        TestMethod(sampleClass);
+
+//        System.Console.WriteLine(sampleClass.IntegerProperty);
+//    }
+
+//    public static void TestMethod(SampleClass sampleClass)
+//    {
+//        sampleClass.IntegerProperty = 2;
+//    }
+//}
+
+//public class SampleClass
+//{
+//    public int IntegerProperty { get; set; }
+//}
