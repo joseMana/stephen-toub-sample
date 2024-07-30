208dea1dabf2f38c8c94c277632ddb60067d8aa4
Joseph <josemanangan@gmail.com>
2024-07-30 14:52:54 -0400

    save

---
+++ StephenToubSample/Polly.cs
@@ -0,0 +1,21 @@
+﻿using Polly;
+using Polly.Retry;
+
+public class ProgramPolly
+{
+    public static async Task Main(String[] args)
+    {
+        ResiliencePipeline pipeline = new ResiliencePipelineBuilder()
+            .AddRetry(new RetryStrategyOptions()) 
+            .AddTimeout(TimeSpan.FromSeconds(10)) 
+            .Build();
+
+        var cts = new CancellationTokenSource();
+
+        await pipeline.ExecuteAsync(async token =>
+        {
+            await Task.Delay(1000, token);
+            Console.WriteLine("Hello World!");
+        }, cts.Token);
+    }
+}
